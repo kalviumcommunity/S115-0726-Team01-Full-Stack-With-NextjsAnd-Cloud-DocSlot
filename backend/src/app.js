cf671514b195503883cfd696f4e3cadf382e728b
Mohammad Aamir <aamir.tbs@gmail.com>
2026-07-08 19:43:53 +0530

    Add Express app bootstrap

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+import routes from "./routes/index.js";
+import errorHandler from "./middleware/errorHandler.js";
+import notFound from "./middleware/notFound.js";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+app.use("/api", routes);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+export default app;
